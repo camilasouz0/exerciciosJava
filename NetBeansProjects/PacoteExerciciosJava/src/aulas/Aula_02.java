d5332b406c24f403d0dd9bf20082433de2c4a2aa
Camila Souza <camilamaiara@hotmail.com.br>
2020-03-24 16:08:13 -0300

    24/03/20

---
+++ NetBeansProjects/PacoteExerciciosJava/src/aulas/Aula_02.java
@@ -0,0 +1,20 @@
+
+package aulas;
+
+public class Aula_02 {
+    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        
+        double c = (double)x/y; //para forçar o resultado com casas decimais
+        
+        System.out.println(c);
+        
+        //outro exemplo seria para forçar "c" ser inteiro
+        
+        int h = (int)c;
+        
+        System.out.println(h);
+    }
+}
